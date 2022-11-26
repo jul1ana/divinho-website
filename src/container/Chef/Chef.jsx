b0813ed7d43f284654bc59c9a10ea1f53fab985f
jul1ana <julianaadt97@gmail.com>
2022-11-26 20:26:15 -0300

    Adding applications performed in the Chef and Founder components

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="founder" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Our Founder" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">I am forever grateful for the gift of my work. Through him, I can make impossible dreams come true.</p>
+        </div>
+        <p className="p__opensans">When I thought of founding a local wine and beverage market, my main focus was to somehow bring joy to people, so why not bring such happiness through taste?</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Oenologist & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
